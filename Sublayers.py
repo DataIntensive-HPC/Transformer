3cf927311b45166a8fda5c12dd155d53f37afb49
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-17 22:02:55 -0700

    a

--- Sublayers.py
+++ Sublayers.py
@@ -102,7 +102,7 @@ def factorized_attention(q_I, W_A, W_B, W_Bt, W_At, qt, v, d_k, mask=None, dropo
     print(v.size())    
 
     #output = torch.matmul(scores, v)
-    output = torch.einsum('kabm,kbmj->kbaj', [scores, v])
+    output = torch.einsum('kabm,kbia->kbim', [scores, v])
 
     print(" output size")
     print(output.size())      

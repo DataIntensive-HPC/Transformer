3676a51838838241efdfeeb5473c04cf0527716b
M.Emin Ozturk <ozturk.27@osu.edu>
2021-02-17 19:27:08 -0700

    factorized_k

--- Models.py
+++ Models.py
@@ -9,12 +9,12 @@ def get_clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
 
 class Encoder(nn.Module):
-    def __init__(self, vocab_size, d_model, N, heads, dropout):
+    def __init__(self, vocab_size, d_model, N, heads, factorized_k, dropout):
         super().__init__()
         self.N = N
         self.embed = Embedder(vocab_size, d_model)
         self.pe = PositionalEncoder(d_model, dropout=dropout)
-        self.layers = get_clones(EncoderLayer(d_model, heads, dropout), N)
+        self.layers = get_clones(EncoderLayer(d_model, heads, factorized_k, dropout), N)
         self.norm = Norm(d_model)
     def forward(self, src, mask):
         x = self.embed(src)
@@ -24,12 +24,12 @@ class Encoder(nn.Module):
         return self.norm(x)
     
 class Decoder(nn.Module):
-    def __init__(self, vocab_size, d_model, N, heads, dropout):
+    def __init__(self, vocab_size, d_model, N, heads, factorized_k, dropout):
         super().__init__()
         self.N = N
         self.embed = Embedder(vocab_size, d_model)
         self.pe = PositionalEncoder(d_model, dropout=dropout)
-        self.layers = get_clones(DecoderLayer(d_model, heads, dropout), N)
+        self.layers = get_clones(DecoderLayer(d_model, heads, factorized_k ,dropout), N)
         self.norm = Norm(d_model)
     def forward(self, trg, e_outputs, src_mask, trg_mask):
         x = self.embed(trg)
@@ -39,10 +39,10 @@ class Decoder(nn.Module):
         return self.norm(x)
 
 class Transformer(nn.Module):
-    def __init__(self, src_vocab, trg_vocab, d_model, N, heads, dropout):
+    def __init__(self, src_vocab, trg_vocab, d_model, N, heads, factorized_k ,dropout):
         super().__init__()
-        self.encoder = Encoder(src_vocab, d_model, N, heads, dropout)
-        self.decoder = Decoder(trg_vocab, d_model, N, heads, dropout)
+        self.encoder = Encoder(src_vocab, d_model, N, heads, factorized_k ,dropout)
+        self.decoder = Decoder(trg_vocab, d_model, N, heads, factorized_k ,dropout)
         self.out = nn.Linear(d_model, trg_vocab)
     def forward(self, src, trg, src_mask, trg_mask):
         e_outputs = self.encoder(src, src_mask)
@@ -56,7 +56,7 @@ def get_model(opt, src_vocab, trg_vocab):
     assert opt.d_model % opt.heads == 0
     assert opt.dropout < 1
 
-    model = Transformer(src_vocab, trg_vocab, opt.d_model, opt.n_layers, opt.heads, opt.dropout)
+    model = Transformer(src_vocab, trg_vocab, opt.d_model, opt.n_layers, opt.heads, opt.factorized_k, opt.dropout)
        
     if opt.load_weights is not None:
         print("loading pretrained weights...")
